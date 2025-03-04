928f983b487caf70523d8c851e5383d244c55402
ja-carroll <james.carroll940@gmail.com>
2025-03-04 01:15:38 +0000

    Working through the action

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
 echo "Running the pr review tool"
+
+cd "${GITHUB_WORKSPACE}" || exit 1
+
+export FILE_PATH="${INPUT_FILEPATH}"
+export CHECKSTYLE_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
+
+if [[ -z "$FILE_PATH" ]]; then
+  echo "Error: FILE_PATH is not set."
+  exit 1
+fi
+
+if [[! -f "$FILE_PATH" ]]; then
+  echo "Error: File not found at $FILE_PATH."
+  exit 1
+fi
+
+exec checkstyle-pr-review
